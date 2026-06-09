ececc428d69ccea4242fa2d305c40f21730e552c
Whsxg <shabanov.matveyka@bk.ru>
2026-06-10 01:59:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-const BOT_TOKEN = '8291475798:AAHMVeNhqwhQKju8dZnYUP3x7LIAu-kld';
+const BOT_TOKEN = '8291475798:AAHMVeNhqwhQKju8dZnYUP3x7LIAu-kldxs';
 const bot = new Telegraf(BOT_TOKEN);
 
 console.log('🤖 Bot initializing...');
